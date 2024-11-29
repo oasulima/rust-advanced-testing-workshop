4ecf9e22c86a60dab8ed3d7b2830c7e32efa3879
Oleg Sulima <oleg.sulima@outlook.com>
2024-11-29 20:22:58 +0000

    (01) better_assertions - (05) enums

--- exercises/01_better_assertions/05_enums/src/lib.rs
+++ exercises/01_better_assertions/05_enums/src/lib.rs
@@ -6,6 +6,7 @@
 mod tests {
     use googletest::assert_that;
     use googletest::matchers::matches_pattern;
+    use googletest::prelude::eq;
 
     #[derive(Debug)]
     enum MyCustomEnum {
@@ -18,18 +19,18 @@ mod tests {
     fn failed_is_b() {
         let x = MyCustomEnum::A;
         // This will become `assert_matches!` once it stabilises!
-        assert!(matches!(x, MyCustomEnum::B(_)));
+        assert_that!(x, matches_pattern!(MyCustomEnum::B(_)));
     }
 
     #[googletest::test]
     fn failed_is_c() {
         let x = MyCustomEnum::B(10);
-        assert!(matches!(x, MyCustomEnum::C { .. }));
+        assert_that!(x, matches_pattern!(MyCustomEnum::C { .. }));
     }
 
     #[googletest::test]
     fn failed_is_c_with_value() {
         let x = MyCustomEnum::B(10);
-        assert!(matches!(x, MyCustomEnum::C { a: "hello" }));
+        assert_that!(x, matches_pattern!(MyCustomEnum::C { a: eq("hello") }));
     }
 }
